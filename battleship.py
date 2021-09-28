e4780e349d0c30bdde1eb75a8c6ad49d68524730
Sneha-kottauppari <89783978+Sneha-kottauppari@users.noreply.github.com>
2021-09-28 13:24:35 +0530

    Update battleship.py

--- battleship.py
+++ battleship.py
@@ -79,8 +79,6 @@ def createShip():
     row_centre= random.randint(1,8)
     col_centre= random.randint(1,8)
     orientation=random.randint(0,1)
-    ship_placement=[[],[],[]]
-    # print(row_centre,col_centre,orientation)
     if orientation==0:
         ship_placement=[[row_centre,col_centre-1],[row_centre,col_centre],[row_centre,col_centre+1]]
     else:
