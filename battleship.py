6c7331bbc5a6d26f0590c0bff58cfef49959f061
Sneha-kottauppari <kottaupparisneha@msitprogram.net>
2021-10-05 11:23:56 +0530

    updateboard function

--- battleship.py
+++ battleship.py
@@ -270,6 +270,11 @@ Parameters: dict mapping strs to values ; 2D list of ints ; int ; int ; str
 Returns: None
 '''
 def updateBoard(data, board, row, col, player):
+    if board[row][col]==SHIP_UNCLICKED:
+        board[row][col]=SHIP_CLICKED
+    if board[row][col]==EMPTY_UNCLICKED:
+        board[row][col]=EMPTY_CLICKED
+
     return
 
 
@@ -366,9 +371,10 @@ def runSimulation(w, h):
 if __name__ == "__main__":
 
     ## Finally, run the simulation to test it manually ##
-    runSimulation(500, 500)
+    # runSimulation(500, 500)
     # test.testMakeModel()
     # test.testIsVertical()
     # test.testIsHorizontal()
     # test.testGetClickedCell()
     # test.testShipIsValid()
+    test.testUpdateBoard()
